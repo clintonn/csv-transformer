c45fc5d8b9bb5af36b7f70fdc547672fec7e68fb
clintonn <clinton.ngyn@gmail.com>
2018-12-09 23:35:13 -0500

    Add a couple more CLI flags and handlers

--- lib/csv_transformer.rb
+++ lib/csv_transformer.rb
@@ -1,27 +1,18 @@
-require 'optparse'
+require 'csv'
 require 'pry'
 
-options = {}
-
-OptionParser.new do |parser|
-  parser.banner = 'Usage: ruby lib/csv_transformer.rb'
-  parser.on(
-    '-o', '--output FILEPATH', 'Path to output relative to the project directory'
-  ) do |val|
-    # TODO: Write example file
+class CSVTransformer
+  def initialize(
+    output: File.open(File.expand_path('output/showtimes.txt', './'), 'w'),
+    input: CSV.read(File.expand_path('input/movies.csv', './'), headers: true),
+    print_results: false,
+    start_date: Date.today
+  )
+    @output = output
+    @input = input
+    @print_results = print_results
   end
 
-  parser.on(
-    '-i', '--input FILEPATH', 'Path to the CSV file to process'
-  ) do |val|
-    # TODO: Resolve file, throw error if unresolvable
+  def transform
   end
-
-  parser.on(
-    '-p', '--print', 'Print the output file to stdout'
-  ) do |val|
-    options[:print] = true
-  end
-end.parse!
-
-puts options[:hello] if options[:hello]
+end
